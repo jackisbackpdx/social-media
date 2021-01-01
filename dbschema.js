9c5252d00e910422666524b526c35e3ef228b4ba
Jackson <jackson.kelley.g@gmail.com>
2020-12-31 16:26:05 -0800

    all of the backend files

---
+++ dbschema.js
@@ -0,0 +1,56 @@
+let db = [ {  
+    users: [
+        {
+            userId: 'dh23ggjSh32g543j5g5f43',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2020-10-25T8:21:30.898Z',
+            imageUrl: 'image/dsfsdkfghskdfhs/dhfdhfd',
+            bio: 'Hello, my name is user, nice to meet you',
+            website: 'https://user.com',
+            location: 'London, UK'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'User',
+            body: 'This is the scream',
+            createdAt: '2020-10-22T02:37:09.693Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ],
+    comments: [
+        {
+            userHandle: 'user',
+            screamId: 'sldkfjsldkjfwji',
+            body: 'nice one mate!',
+            createdAt: '2019-03-15T10:59:52.748Z'
+        }
+    ]
+}
+]
+
+const userDetails = {
+    //Redux data
+    credentials: {
+        userId: 'N43JSH234LKJFLSKJD49LKDJ',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2019-03-15T10:34:45',
+        imageUrl: 'image/dsfsdkfghsldk/sdklfjoiej',
+        bio: 'Hello, my name is user, nice to meet you',
+        website: 'https://user.com',
+        location: 'London, UK'
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'hh38dslkksdfkkdsf98309d',
+        },
+        {
+            userHandle: 'user',
+            screamId: 'hh38dslksdfewff98309d',
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: 'AIzaSyCrv0XU337XUnBFKTH5_FlInbcRGv4YZyI',
+    authDomain: 'socialape-45fc1.firebaseapp.com',
+    databaseURL: 'https://socialape-45fc1.firebaseio.com',
+    projectId: 'socialape-45fc1',
+    storageBucket: 'socialape-45fc1.appspot.com',
+    messagingSenderId: '1007116978825',
+    appId: '1:1007116978825:web:a2457a1c71fb0d02b05458',
+    measurementId: 'G-YLPBZV1EKQ'
+};
